104492ea0db1677bd712874d178716c54b59a96c
Guilherme <guilherme.oliveirasantos@hotmail.com>
2023-02-15 23:55:39 -0300

    Section About finalizada

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,20 +11,20 @@ const Nav = () => {
     <nav className='fixed bottom-2 lg:botton-8 w-full overflow-hidden z-50'>
         <div className="container mx-auto">
           {/* nav inner*/}          
-          <div className='w-full bg-black/20 h-[80px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-9 flex justify-between items-center text-2x1 text-white/50'>
-            <Link to='home'activeClass='active' smooth={true} spy={true} offset={-200} className='cursor-pointer w-[60px] h-[60px] flex items-center justfy-center'>
+          <div className='w-full bg-black/20 h-[80px] backdrop-blur-2x1 rounded-full max-w-[460px] mx-auto px-7 flex justify-between items-center text-2x1 text-white/50'>
+            <Link to='home'activeClass='active' smooth={true} spy={true} offset={-200} className='cursor-pointer w-[50px] h-[50px] flex items-center justfy-center'>
               <BiHomeAlt />
             </Link >
-            <Link to='about' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[60px] h-[60px] flex items-center justfy-center'>
+            <Link to='about' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[50px] h-[50px] flex items-center justfy-center'>
               <BiUser />
             </Link>
-            <Link to='services' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[60px] h-[60px] flex items-center justfy-center'>
+            <Link to='services' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[50px] h-[50px] flex items-center justfy-center'>
               <BsClipboardData />
             </Link>
-            <Link to='work' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[60px] h-[60px] flex items-center justfy-center'>
+            <Link to='work' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[50px] h-[50px] flex items-center justfy-center'>
               <BsBriefcase />
             </Link>
-            <Link to='contact' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[60px] h-[60px] flex items-center justfy-center'>
+            <Link to='contact' activeClass='active' smooth={true} spy={true} className='cursor-pointer w-[50px] h-[50px] flex items-center justfy-center'>
               <BsChatSquareText />
             </Link>
           </div>          
